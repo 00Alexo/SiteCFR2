a97cf2c44ad1aa58ac1c2baa44105ba3d595c30b
Alexo <alexsuciultc200@gmail.com>
2023-08-06 18:24:25 +0300

    da

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const selectorNewsBox3 = document.getElementById('selectorNewsBox3');
 const selectorNewsBox4 = document.getElementById('selectorNewsBox4');
 const selectorNewsBox5 = document.getElementById('selectorNewsBox5');
 const selectorNewsBox = document.querySelectorAll('.selectorNewsBox');
+const selectNewsBox = document.getElementById('selectNewsBox');
+const newsBox = document.querySelector('.newsBox');
 const timer = document.getElementById('timer');
+const homeTeam = document.getElementById('homeTeam');
+const awayTeam = document.getElementById('awayTeam');
+
 let oraMeci = 21;
 let minutulMeci = 30;
 let secundaMeci = 0;
+let timerActiv;
 
 let Timer = () => {
     const currentData = new Date();
@@ -55,9 +61,32 @@ selectorNewsBox5.addEventListener('click', () => {
     selectorNewsBox5.style.fill = 'black';
 });
 
-setInterval(() => {
+timerActiv = setInterval(() => {
     const { oraTimer, minutulTimer, secundaTimer } = Timer();
     if (oraTimer != 0 || minutulTimer != 0 || secundaTimer != 0) {
         timer.innerHTML = `${oraTimer} : ${minutulTimer} : ${secundaTimer}`;
     }
 }, 1000);
+
+const page2 = () =>{
+    clearInterval(timerActiv);
+    timer.innerHTML = "2 - 1";
+    homeTeam.style.opacity = 0;
+    awayTeam.style.opacity = 0;
+    setTimeout(() => {
+    homeTeam.src="Home/IconADANA.png";
+    awayTeam.src="Home/IconCFR.png";
+    setTimeout(() =>{  
+        homeTeam.style.opacity = 1;
+        awayTeam.style.opacity = 1;
+        }, 1000);
+    }, 500);
+    newsBox.style.backgroundImage = "url('Home/ADANAvCFR.jpg')";
+    homeTeam.style.marginLeft= "-220px";  
+
+}
+selectorNewsBox2.addEventListener('click', () =>{
+    page2();
+});
+
+let page2Interval = setInterval(page2, 10000);
